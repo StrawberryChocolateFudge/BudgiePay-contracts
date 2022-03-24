54fe5e407f6eae760fe60ac9860e3222ba917196
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-03-24 18:42:25 +0000

    removed unused deps

--- contracts/Payments.sol
+++ contracts/Payments.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "hardhat/console.sol";
 struct Payment {
     uint256 id;
     address from;
